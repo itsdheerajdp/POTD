c0a3ee5d23169b62d1675724607807f967dc2a59
Dheeraj Singh <singhdheeraj2696@gmail.com>
2024-08-20 23:23:34 +0530

    plit an array into two equal Sum subarrays

---
+++ GFG/18-08-2024.cpp
@@ -0,0 +1,19 @@
+// Split an array into two equal Sum subarrays
+class Solution {
+  public:
+    bool canSplit(vector<int>& arr) {
+        // code here
+        int sum = accumulate(arr.begin(),arr.end(),0);
+        if(sum%2!=0)
+            return false;
+        sum/=2;
+        int curr = 0;
+        for(int i=0;i<arr.size();i++){
+            curr+=arr[i];
+            if(curr==sum)
+                return true;
+        }
+        return false;
+    
+    }
+};
